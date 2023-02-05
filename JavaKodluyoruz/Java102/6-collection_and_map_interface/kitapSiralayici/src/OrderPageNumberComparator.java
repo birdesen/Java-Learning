0defd698583852657e5f51acdac996a4c0aabab6
birdesen <87832209+birdesen@users.noreply.github.com>
2023-02-05 20:47:05 +0300

    Delete JavaKodluyoruz/Java102/6-collection_and_map_interface/kitapSiralayici directory

--- JavaKodluyoruz/Java102/6-collection_and_map_interface/kitapSiralayici/src/OrderPageNumberComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class OrderPageNumberComparator implements Comparator<Book> {
-    
-    @Override
-    public int compare(Book p1, Book p2) {
-        return p1.getPageNumber() - p2.getPageNumber();
-    }
-}
