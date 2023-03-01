bb8fe0af56e6b2080d8822f07dba697ea546858d
birdesen <87832209+birdesen@users.noreply.github.com>
2023-03-01 20:42:24 +0300

    Delete JavaKodluyoruz/Java102/6-collection_and_map_interface/treeMap directory

--- JavaKodluyoruz/Java102/6-collection_and_map_interface/treeMap/src/OrderByNoComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class OrderByNoComparator implements Comparator<Integer> {
-    @Override
-    public int compare(Integer o1, Integer o2) {
-        return o1 - o2;
-    }
-}
