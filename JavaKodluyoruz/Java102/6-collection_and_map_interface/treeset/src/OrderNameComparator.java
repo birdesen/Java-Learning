295cde0ed7b6e3b422247c701e2bf3ee5a766e30
birdesen <87832209+birdesen@users.noreply.github.com>
2023-03-01 20:42:17 +0300

    Delete JavaKodluyoruz/Java102/6-collection_and_map_interface/treeset directory

--- JavaKodluyoruz/Java102/6-collection_and_map_interface/treeset/src/OrderNameComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class OrderNameComparator implements Comparator<Student> {
-    @Override
-    public int compare(Student o1, Student o2) {
-        return o1.getName().compareTo(o2.getName());
-    }
-}
